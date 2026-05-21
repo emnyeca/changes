5d3c3bb81819e7bc988fccede0b6bf98d5052818
Emnyeca <emnyeca@gmail.com>
2026-05-21 00:27:02 -0700

    Create __init__.py

---
+++ src/harmony_cloud/__init__.py
@@ -0,0 +1,6 @@
+"""
+Harmony Cloud package initialization.
+
+This package contains modules for generating six-voice jazz chord clouds,
+voice leading, chord parsing, MIDI writing, and integration with Elektron Digitone II.
+"""
